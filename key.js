41b3f7d5443d5982a3b93d24514c799b5dd5d82d
faizp113 <FaizP113@gmail.com>
2017-05-14 18:52:21 -0500

    Add files via upload

---
+++ key.js
@@ -0,0 +1,8 @@
+//console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'yrwwEP8jEf5Ag06WFh7QcNhjA',
+  consumer_secret: 'nZRfQA1dB1VbqES8U6SncIDbYfCzo2YkZyOdkiis0UkwkbPODO',
+  access_token_key: '	27172044-Bg2MraNr3C4MSYy38rmJnLMczChBY92ipD8ccBRV5',
+  access_token_secret: '8R0YZWN4Nm8yYkvCCAk5Xon2sMxt8PisuPj9kLXNsKhB7',
+}
